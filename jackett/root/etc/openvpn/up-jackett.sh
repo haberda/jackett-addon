2131dc7b69f9a6cb961ef0f412b8c2ed16945c83
haberda <haberda@gmail.com>
2021-06-15 12:02:25 -0700

    Update to ghcr.io workflow

---
+++ jackett/root/etc/openvpn/up-jackett.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+declare CONFIG
+
+export localNet="192.168.1.0/24"
+
+eval $(/sbin/ip route list match 0.0.0.0 | awk '{if($5!="tun0"){print "GW="$3"\nINT="$5; exit}}')
+
+echo "adding route to local network ${localNet} via ${GW} dev ${INT}"
+
+/sbin/ip route del "$localNet" || echo "Error deleting localNet rule, attempting to continue"
+
+/sbin/ip route add "${localNet}" via "${GW}" dev "${INT}" || echo "Error adding localNet rule, attempting to continue. You may not be able to access Transmission from the LAN"
+
+#exec s6-setuidgid user mono /app/Jackett/JackettConsole.exe
+exec s6-setuidgid abc /app/Jackett/jackett
