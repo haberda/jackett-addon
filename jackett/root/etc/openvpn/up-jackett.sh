d4963960d54bbb0c01b4c44cc395c1a8ab3a9b2c
Daniel Haber <haberda@gmail.com>
2024-03-15 09:17:00 -0700

    Possible routing fix

--- jackett/root/etc/openvpn/up-jackett.sh
+++ jackett/root/etc/openvpn/up-jackett.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-declare CONFIG
+#declare CONFIG
 
-export localNet=$(bashio::config 'local_network')
+#export localNet=$(bashio::config 'local_network')
 
-eval $(/sbin/ip route list match 0.0.0.0 | awk '{if($5!="tun0"){print "GW="$3"\nINT="$5; exit}}')
+#eval $(/sbin/ip route list match 0.0.0.0 | awk '{if($5!="tun0"){print "GW="$3"\nINT="$5; exit}}')
 
-echo "adding route to local network ${localNet} via ${GW} dev ${INT}"
+#echo "adding route to local network ${localNet} via ${GW} dev ${INT}"
 
-/sbin/ip route del "$localNet" || echo "Error deleting localNet rule, attempting to continue"
+#/sbin/ip route del "$localNet" || echo "Error deleting localNet rule, attempting to continue"
 
-/sbin/ip route add "${localNet}" via "${GW}" dev "${INT}" || echo "Error adding localNet rule, attempting to continue. You may not be able to access Transmission from the LAN"
+#/sbin/ip route add "${localNet}" via "${GW}" dev "${INT}" || echo "Error adding localNet rule, attempting to continue. You may not be able to access Transmission from the LAN"
 
-#exec s6-setuidgid user mono /app/Jackett/JackettConsole.exe
 exec s6-setuidgid abc /app/Jackett/jackett
